0df543384d364b306860b543aef206c330b2d99e
experiment4life <experiment4life@users.noreply.github.com>
2023-03-14 11:44:41 -0500

    add coverage support

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,55 +5,84 @@ import numpy.testing as npt
 import pytest
 
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
+# def test_daily_mean_zeros():
+#     """Test that mean function works for an array of zeros."""
+#     from inflammation.models import daily_mean
+#
+#     test_input = np.array([[0, 0],
+#                            [0, 0],
+#                            [0, 0]])
+#     test_result = np.array([0, 0])
+#
+#     # Need to use Numpy testing functions to compare arrays
+#     npt.assert_array_equal(daily_mean(test_input), test_result)
+#
+#
+# def test_daily_mean_integers():
+#     """Test that mean function works for an array of positive integers."""
+#     from inflammation.models import daily_mean
+#
+#     test_input = np.array([[1, 2],
+#                            [3, 4],
+#                            [5, 6]])
+#     test_result = np.array([3, 4])
+#
+#     # Need to use Numpy testing functions to compare arrays
+#     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+
+def test_daily_mean(test,expected):
     from inflammation.models import daily_mean
-
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
 
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
+# def test_daily_max():
+#     """ Testing the max value with positive integers"""
+#     from inflammation.models import daily_max
+#
+#     test_input = np.array([[7, 8, 9],
+#                            [1, 2, 3],
+#                            [3, 4, 5]])
+#
+#     test_result = np.array([7, 8, 9])
+#
+#     npt.assert_array_equal(daily_max(test_input), test_result)
 
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+@pytest.mark.parametrize(
+    "test,expected",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+        ([[4, 2, 5], [1, 6, 2], [4, 1, 9]], [4, 6, 9]),
+        ([[4, -2, 5], [1, -6, 2], [-4, -1, 9]], [4, -1, 9]),
+    ])
 
-def test_daily_max():
+def test_daily_max(test,expected):
     """ Testing the max value with positive integers"""
     from inflammation.models import daily_max
 
-    test_input = np.array([[7, 8, 9],
-                           [1, 2, 3],
-                           [3, 4, 5]])
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
-    test_result = np.array([7, 8, 9])
+@pytest.mark.parametrize(
+    "test,expected",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+        ([[4, 2, 5], [1, 6, 2], [4, 1, 9]], [1, 1, 2]),
+        ([[4, -2, 5], [1, -6, 2], [-4, -1, 9]], [-4, -6, 2]),
+    ])
 
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-
-def test_daily_min():
+def test_daily_min(test,expected):
     """Testing the daily minimum using positive and negative integers"""
     from inflammation.models import daily_min
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
-    test_input = np.array([[-6, 7, -11],
-                           [4, -4, 6],
-                           [1, 3, 2]])
-
-    test_result = np.array([-6, -4, -11])
-
-    npt.assert_array_equal(daily_min(test_input), (test_result))
 
 def test_daily_minimum_string():
     """ Tesing for type errors when passing in strings"""
